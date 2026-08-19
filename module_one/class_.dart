096837c76899214a9c076201b2fb2fd1bf9fefe6
Ibrahim-K98han <aitvetibrahim@gmail.com>
2026-08-19 09:00:46 +0600

    final and const discussion

--- module_one/class_.dart
+++ module_one/class_.dart
@@ -1,11 +1,17 @@
 main() {
-  print("Hello World");
-  print("Hello");
-
   String name = "John Doe";
   int age = 30;
   double amount = 100.50;
   bool isActive = true;
+  name = "Jane Smith";
 
-  print(name);
+  // print(
+  //     "$name is $age years old and has an amount of $amount. Active status: $isActive");
+
+  final c = 25; // run-time constant
+
+  const pi = 3.14; // compile-time constant
+
+  final currentTime = DateTime.now(); // run-time constant
+  print("Current time: $currentTime");
 }
